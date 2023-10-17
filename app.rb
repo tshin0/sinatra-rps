421d819c5ea4e51b4d6cd48b105f56af69aeb260
tshin0 <146213491+tshin0@users.noreply.github.com>
2023-10-17 21:02:52 +0000

    added rules

--- app.rb
+++ app.rb
@@ -2,8 +2,26 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
+end
+
+get("/rock") do
+  @my_input = 1
+  @player_input = rand(1..3)
+
+  erb(:rock)
+end
+
+get("/paper") do
+  @my_input = 2
+  @player_input = rand(1..3)
+  
+  erb(:paper)
+end
+
+get("/scissors") do
+  @my_input = 3
+  @player_input = rand(1..3)
+
+  erb(:scissors)
 end
